9c64b4791af3bddaca1cd0a10156695fe87b6785
terry cadd <terry.cadd@gmail.com>
2019-07-01 02:20:33 +0100

    writing a history of moves and time travel ability

--- src/index.js
+++ src/index.js
@@ -108,20 +108,30 @@ class Board extends React.Component {
 
 
 class Game extends React.Component {
-  render() {
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
-    );
-  }
+    constructor(props) {
+	super(props);
+	this.state = {
+	    history: [{
+		squares: Array(9).fill(null),
+	    }],
+	    xIsNext: true,
+	};
+    }
+    render() {
+	return (
+		<div className="game">
+		<div className="game-board">
+		<Board />
+		</div>
+		<div className="game-info">
+		<div>{/* status */}</div>
+		<ol>{/* TODO */}</ol>
+		</div>
+		</div>
+	);
+    }
 }
+
 
 
 
