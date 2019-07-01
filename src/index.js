ba8542df14febadece6494639db8612b71cf1c6b
terry cadd <terry.cadd@gmail.com>
2019-07-01 02:04:11 +0100

    created tic tac toe example detects when there is a winner
    possible to click on same square more than once.
    
    square component
     render method
      render() { return ... }
    
    ... is html jsx abomination ...
     in this case is a button component with className of square .
     button has onClick event ,
       onClick = { () => this.props.onClick() }
    
      button text is
         { this.props.value  }
    
    props is really attribute of object talking about.
    
    ---------------------------------------------------
    
    overall
    
        game  -> render ()
            board -> render ()
                  square(i) -> render ()
    	      0 to 8 or something
    
    --------------------------------------------------
    
    some mention of functional programming being more useful as
    less side effects
    
    could write minesweeper in javascript

--- src/index.js
+++ src/index.js
@@ -15,22 +15,14 @@ outside at toplevel
 */
 
 class Square extends React.Component {
-    constructor(props) {
-	super(props);  // call superclass
-	this.state = {
-	    value: null,
-	};
-    }
-
-    
     render() {
 	return (
 	    // arrow function syntax 
 		<button
 	    className="square"
-	    onClick={ () => this.setState({value: 'X'})}
+	    onClick={ () => this.props.onClick() }
 		>
-		{ this.state.value }
+		{ this.props.value }
 	    </button>
 	);
     }
@@ -48,6 +40,8 @@ make an 3x3 board or 9 cell array
 
 squares  : [null,null,null,null,null,null,null,null,null]
 
+squares.slice () 
+
 */
 
 class Board extends React.Component {
@@ -55,39 +49,60 @@ class Board extends React.Component {
 	super(props);
 	this.state = {
 	    squares: Array(9).fill(null),
+	    xIsNext: true,
 	};
     }
 
+    
+
+    handleClick(i) {
+	const squares = this.state.squares.slice();
+	squares[i] = this.state.xIsNext ? 'X' : 'O';
+	this.setState({
+	    squares: squares,
+	    xIsNext: !this.state.xIsNext,
+	});
+    }
+
+    
     renderSquare(i) {
-	return <Square value={i}/>;
+	return <Square
+	value={this.state.squares[i]}
+	onClick={ () => this.handleClick(i) }
+	    />;
     }
 
     render() {
-	const status = 'Next player: X';
-
+	const winner = calculateWinner(this.state.squares);
+	let status;
+	if (winner) {
+	    status = 'Winner: ' + winner;
+	} else {
+	    status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+	}
+	
 	return (
 		<div>
 		<div className="status">{status}</div>
 		<div className="board-row">
 		{this.renderSquare(0)}
-            {this.renderSquare(1)}
-            {this.renderSquare(2)}
-            </div>
+	    {this.renderSquare(1)}
+	    {this.renderSquare(2)}
+	    </div>
 		<div className="board-row">
 		{this.renderSquare(3)}
-            {this.renderSquare(4)}
-            {this.renderSquare(5)}
-            </div>
+	    {this.renderSquare(4)}
+	    {this.renderSquare(5)}
+	    </div>
 		<div className="board-row">
 		{this.renderSquare(6)}
-            {this.renderSquare(7)}
-            {this.renderSquare(8)}
-            </div>
+	    {this.renderSquare(7)}
+	    {this.renderSquare(8)}
+	    </div>
 		</div>
 	);
     }
 }
-
 
 
 
@@ -119,4 +134,27 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
 
